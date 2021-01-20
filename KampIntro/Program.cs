5f6d2b7f09f494e61ef3a3afc7b63d3714c0150c
Batuhan Tryk <batuhan_tryk@hotmail.co.uk>
2021-01-20 20:12:46 +0300

    16 Ocak Ders Sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini Tekrarlama
+            //değer tutucu,alias
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizorani = 1.45;
+            bool SistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (SistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
